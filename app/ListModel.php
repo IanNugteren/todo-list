af3ba46c5ab10de1fbc41fba2ca87c3ede4e8089
IanNugteren <99045693@mydavinci.nl>
2019-03-15 23:12:49 +0100

    task relation has been made

--- app/ListModel.php
+++ app/ListModel.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ListModel extends Model
 {
     protected $table = 'lists';
+
+    public function tasks ()
+    {
+        return $this->hasMany('App\Task', 'list_id');
+    }
 }
